3b424f69e986d436f3732f16c4fc334dfe973a85
Tiago <venceslau.tiago@gmail.com>
2025-01-31 21:36:50 +0000

    DECAF-89 initial setup
    Signed-off-by: Tiago <venceslau.tiago@gmail.com>

--- src/CliWrapper.ts
+++ src/CliWrapper.ts
@@ -111,7 +111,7 @@ export class CliWrapper {
       file = path.join(basePath, file);
       if (fs.statSync(file).isDirectory()) {
         accum.push(...this.crawl(file, levels - 1));
-      } else if (file.match(new RegExp(`${CLI_FILE_NAME}.[cm]?js`, "gm"))) {
+      } else if (file.match(new RegExp(`${CLI_FILE_NAME}.[cm]?js$`, "gm"))) {
         // } else if (file.endsWith(`${CLI_FILE_NAME}.cjs`)) {
         accum.push(file);
       } else {

de2732eb7d53358eff843cc3c966e56157f1d7ea
Tiago <venceslau.tiago@gmail.com>
2025-01-31 01:59:19 +0000

    DECAF-89 update
    Signed-off-by: Tiago <venceslau.tiago@gmail.com>

--- src/demo/cli-module.ts
+++ src/demo/cli-module.ts
@@ -4,14 +4,21 @@ import { Command } from "commander";
  * @description demo CLI module for libraries
  * @summary minimal implementation on how to extend the decaf-ts cli
  *
- * Simply logs a message
+ * - The name of the returned function will be considered to be the base command;
+ * - the command you return will be considered as a subCommand and will not be called directly
+ *
+ * @returns {Command} the subCommand to be added to the main decaf CLI
  *
  * @function demo
  *
  * @category CLI
- * @memberOf module:CLI.cli
+ *
+ * @example
+ * `npx decaf demo command "something something"`
+ *
+ * will output `executed demo command with type variable: something something`
  */
-export default function demo() {
+export default function demo(): Command {
   return new Command()
     .command("command <type>")
     .description("A demo command")
